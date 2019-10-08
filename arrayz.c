640e10b05da7835d894dd73813794723367b1fef
Nils Brugger <nbrugger@student.tgm.ac.at>
2019-10-08 14:20:00 +0200

    Pointer & malloc fixes

--- arrayz.c
+++ arrayz.c
@@ -4,6 +4,7 @@
 
 #include "arrayz.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct {
@@ -15,29 +16,34 @@ typedef struct {
     list_entry* start;
     int size;
 } list;
+
+
 typedef void (*each_callback)(list_entry*, int);
 
 list* init_list(){
-    list lst;
-    lst.start = NULL;
-    lst.size = 0;
-    return &lst;
+    list* lst = malloc(sizeof(list));
+    lst->start = NULL;
+    lst->size = 0;
+    return lst;
 }
 
 void add_to_list(list* lst, void* value){
     list_entry* curent = lst->start;
     if(curent == NULL) {
-        list_entry frst;
-        frst.next = NULL;
-        frst.value = value;
-        lst->start = &frst;
+        list_entry* frst = malloc(sizeof(list_entry));
+        frst->next = NULL;
+        frst->value = value;
+        lst->start = frst;
         return;
     }
     while(curent->next != NULL){
         curent = curent->next;
     }
-    list_entry* nxt = (list_entry*)(curent->next);
-    nxt->value = value;
+    list_entry* frst = malloc(sizeof(list_entry));
+    frst->next = NULL;
+    frst->value = value;
+
+    curent->next = frst;
 }
 
 void for_each(list* lst,each_callback code){
@@ -53,9 +59,9 @@ void for_each(list* lst,each_callback code){
 
 
 
-void prnt_entry(list_entry* val,int caunt){
-    //printf(val);
-    printf("Entry : %p \n",val);
+void prnt_entry(list_entry* val,int i){
+    int* some = ((*val).value);
+    printf("Entry : %d \n",*some);
 }
 int main(void){
     list* myList = init_list();
@@ -63,6 +69,7 @@ int main(void){
 
     add_to_list(myList,&val1);
     add_to_list(myList,&val2);
+    add_to_list(myList,&val3);
     add_to_list(myList,&val3);
 
     each_callback each_code = &prnt_entry;
